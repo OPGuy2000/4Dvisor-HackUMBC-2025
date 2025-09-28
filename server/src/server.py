7e0f0eeff912b1c72b9844c96892a90faac8fe02
Prakhar Thapliyal <thapliyal.prakhar@gmail.com>
2025-09-28 04:43:56 -0400

    Adding 4 year plan backend

--- server/src/server.py
+++ server/src/server.py
@@ -3,15 +3,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def students():
     return jsonify(core.list_students())
 
-@app.route("/plan", methods=["GET"])
-def api_plan():
-    sid = request.args.get("sid")
-    major = request.args.get("major")
-    return jsonify(core.four_year_plan(sid, major))
+
+@app.route("/student/<sid>")
+def student_exists(sid):
+    return jsonify(core.get_student(sid))
+
+
+@app.route("/plan/<sid>", methods=["GET"])
+def api_plan(sid):
+    return jsonify(core.four_year_plan(sid))
+
 
 @app.route("/switch", methods=["GET"])
 def api_switch():
@@ -19,17 +25,20 @@ def api_switch():
     new_major = request.args.get("new_major")
     return jsonify(core.switch_major(sid, new_major))
 
+
 @app.route("/clubs", methods=["GET"])
 def api_clubs():
     sid = request.args.get("sid")
     category = request.args.get("category")
     return jsonify(core.suggest_clubs(sid, category))
 
+
 @app.route("/repo", methods=["POST"])
 def api_repo():
     sid = request.form["sid"]
     url = request.form["url"]
     return jsonify(core.add_repo(sid, url))
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
